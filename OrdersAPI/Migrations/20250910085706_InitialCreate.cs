db7be4c52ccd9446f79b0bf1225d57ebb16e9da9
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-09-10 05:04:08 -0400

    Add Migrations + Update Database

---
+++ OrdersAPI/Migrations/20250910085706_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrdersAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "Amount", "CreatedAt", "CustomerName", "Status" },
+                values: new object[,]
+                {
+                    { 1, 100, new DateTime(2025, 9, 10, 0, 0, 0, 0, DateTimeKind.Local), "Alex", "Pending" },
+                    { 2, 75, new DateTime(2025, 9, 10, 0, 0, 0, 0, DateTimeKind.Local), "Damon", "Completed" },
+                    { 3, 65, new DateTime(2025, 9, 10, 0, 0, 0, 0, DateTimeKind.Local), "Stefan", "Not processed" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
