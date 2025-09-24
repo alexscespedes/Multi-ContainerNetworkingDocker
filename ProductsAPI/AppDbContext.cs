e0601f5ba4aeca8edf04e09b7de5e0ccaaa94b10
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-09-24 05:49:52 -0400

    Add HasPrecision for Price Property

--- ProductsAPI/AppDbContext.cs
+++ ProductsAPI/AppDbContext.cs
@@ -13,6 +13,10 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(18, 2);
+
         modelBuilder.Entity<Product>().HasData(
             new Product { Id = 1, Name = "Keyboard", Description = "Mechanical keyboard", Price = 79.99M, InStock = true },
             new Product { Id = 2, Name = "Mouse", Description = "Wireless mouse", Price = 29.99M, InStock = true }
